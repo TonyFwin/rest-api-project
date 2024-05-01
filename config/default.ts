ebcf273d3613cdd6775c8b4a39f5f9efbf5fa7c1
Tony Nguyen <tony.nguyen@modcreativeinc.com>
2024-05-01 11:07:45 -0500

    Add config values for tokens

--- config/default.ts
+++ config/default.ts
@@ -5,4 +5,8 @@ export default {
   port: process.env['PORT'],
   dbUri: process.env['MONGO_URI'],
   saltWorkFactor: process.env['SALT_WORK_FACTOR'],
+  accessTokenTTL: process.env['ACCESS_TOKEN_TTL'],
+  refreshTokenTTL: process.env['REFRESH_TOKEN_TTL'],
+  publicKey: process.env['ACCESS_TOKEN_PUBLIC_KEY'],
+  privateKey: process.env['ACCESS_TOKEN_PRIVATE_KEY'],
 }
