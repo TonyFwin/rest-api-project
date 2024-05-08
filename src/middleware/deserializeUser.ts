5fd3340474fe812ffc4fa88da7405f589b2614ef
Tony Nguyen <tony.nguyen@modcreativeinc.com>
2024-05-08 11:18:15 -0500

    Add update and reissue access token methods to session service

--- src/middleware/deserializeUser.ts
+++ src/middleware/deserializeUser.ts
@@ -1,22 +1,43 @@
 import { get } from 'lodash'
-import { NextFunction, Request, Response } from 'express'
+import { Request, Response, NextFunction } from 'express'
 import { verifyJwt } from '../utils/jwt.utils'
+import { reIssueAccessToken } from '../service/session.service'
 
-const deserializeUser = (req: Request, res: Response, next: NextFunction) => {
+const deserializeUser = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   const accessToken = get(req, 'headers.authorization', '').replace(
     /^Bearer\s/,
     ''
   )
+
+  const refreshToken = get(req, 'headers.x-refresh')
+
   if (!accessToken) return next()
 
   const { decoded, expired } = verifyJwt(accessToken)
 
-  console.log('decoded', decoded)
   if (decoded) {
     res.locals.user = decoded
     return next()
   }
 
+  if (expired && refreshToken) {
+    const newAccessToken = await reIssueAccessToken({
+      refreshToken: refreshToken as string,
+    })
+
+    if (newAccessToken) {
+      res.setHeader('x-access-token', newAccessToken)
+    }
+
+    const result = verifyJwt(newAccessToken as string)
+
+    res.locals.user = result.decoded
+    return next()
+  }
   return next()
 }
 
